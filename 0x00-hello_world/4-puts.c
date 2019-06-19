0cd289202f2541c3b8a40e1e323d393eeaa1cbbc
untaljacko <untaljacko@gmail.com>
2019-06-19 13:28:50 -0500

    new file: task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - This function prints exactly a string
+ * Return: 0 so just print the text.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
