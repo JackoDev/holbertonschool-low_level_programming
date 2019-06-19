557dec456a0c390a005dfb955137d302537e7509
untaljacko <untaljacko@gmail.com>
2019-06-19 15:40:24 -0500

    new file: task 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * main - prints a sytring to the standar error
+ * Return: 1
+ */
+int main(void)
+{
+char texto[60]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *punt=texto;
+	write(2, punt, 60);
+	return (1);
+}
