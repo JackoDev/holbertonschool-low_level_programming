384347b68bc9acb564c9edb837fbfb73334879e3
untaljacko <untaljacko@gmail.com>
2019-06-19 13:40:04 -0500

    new file: task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - This function prints exactly a string with printf
+ * Return: 0 so just print the text.
+ */
+int main(void)
+{
+	printf("%S\n","with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
