cb4815f702bfc5904919a79822d1dc4a5ec65f00
untaljacko <untaljacko@gmail.com>
2019-06-27 10:54:42 -0500

    new file: task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: the int 1 to review
+ * @b: the int 2 to review
+ * Return: 1 if c is a digit
+ */
+int mul(int a, int b)
+{
+	int r = 0;
+
+	r = a * b;
+	return (r);
+}
