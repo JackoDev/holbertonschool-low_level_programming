00148c99665522099bffb6ff22ee9276ac57e3a1
untaljacko <untaljacko@gmail.com>
2019-06-27 19:23:25 -0500

    new file: task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * print_triangle -  draws a triangle on the terminal.
+ * @size: the n times to print the \
+ *
+ * Return: its a void function.
+ */
+void print_triangle(int size)
+{
+	int c = 1;
+	int i = size - 1;
+
+	if (size > 0)
+	{
+		while (c <= size)
+		{
+			while (i >= c)
+			{
+				_putchar(' ');
+				i--;
+			}
+
+			i = 0;
+
+			while (i < c)
+			{
+				_putchar('#');
+				i++;
+			}
+			_putchar('\n');
+			i = size - 1;
+			c++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
