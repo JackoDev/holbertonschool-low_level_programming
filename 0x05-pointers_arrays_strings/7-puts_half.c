0dfc263783db8dce2c3822befd670fb56a7e58b3
untaljacko <untaljacko@gmail.com>
2019-07-02 13:22:02 -0500

    new file: task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string.
+ * @s: the pointer to a first integer
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ * @str: the pointer to the string
+ * Return: its a void function
+ */
+void puts_half(char *str)
+{
+	int c = (_strlen(str));
+	int n;
+
+	if ((c % 2) == 0)
+	{
+		n = (c / 2);
+		while (str[n] != '\0')
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	else
+	{
+		n = ((c - 1) / 2);
+		while (str[n] != '\0')
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	_putchar('\n');
+}
