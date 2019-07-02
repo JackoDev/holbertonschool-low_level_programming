edf39157caa173ac739275829fa25e86639f881e
untaljacko <untaljacko@gmail.com>
2019-07-02 12:35:40 -0500

    fixed: file 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,21 @@
 #include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: the pointer to a first integer
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
 /**
  * print_rev - prints a string in reverse, followed by a new line.
  * @s: the pointer to the string
@@ -7,17 +24,13 @@
  */
 void print_rev(char *s)
 {
-	int c = 0;
-
-	while (s[c] != '\0')
-	{
-		c++;
-	}
+	int c = _strlen(s) - 1;
 
 	while (c >= 0)
 	{
 		_putchar(s[c]);
 		c--;
 	}
+
 	_putchar('\n');
 }
