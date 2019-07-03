0e16455ac8f447d2e4946780d8b6fabb01087387
untaljacko <untaljacko@gmail.com>
2019-07-03 14:13:49 -0500

    fixed: file 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,35 +1,34 @@
 #include "holberton.h"
 /**
- * rev_string - prints a string in reverse, followed by a new line.
- * @s: the pointer to the string
- *
- * Return: its a void function
+ * _sterlen - give the len of string
+ * @s: parameter
+ * Return: return len
+ */
+int _sterlen(char *s)
+{
+	int i, count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	count++;
+
+	return (count);
+}
+/**
+ * rev_string - print rev string
+ * @s: parameter
  */
 void rev_string(char *s)
 {
-	int i;
-	int j = 0;
-	int cont = 0;
+	int start;
+	int len;
+	char a, z;
 
-	for (i = 0; s[i] != '\0'; i++)
+	len = _sterlen(s) - 1;
+	for (start = 0; start < len; start++)
 	{
-		cont++;
+		a = s[start];
+		z = s[len];
+		s[start] = z;
+		s[len--] = a;
 	}
-	cont = i - 1;
-	char new[cont];
-
-	while (cont >= 0)
-	{
-		new[j] = s[cont];
-		cont--;
-		j++;
-	}
-
-	while (j >= 0)
-	{
-		s[cont] = new[cont];
-		cont++;
-		j--;
-	}
-	s[cont + 1] = '\0';
 }
