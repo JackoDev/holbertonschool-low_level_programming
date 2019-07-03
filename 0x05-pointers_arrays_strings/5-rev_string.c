466e2a2d49e6e7a443749ba7801e48dc532b39c0
untaljacko <untaljacko@gmail.com>
2019-07-02 21:16:14 -0500

    fixed: file 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * print_rev - prints a string in reverse, followed by a new line.
+ * @s: the pointer to the string
+ *
+ * Return: its a void function
+ */
+void print_rev(char *s)
+{
+	int i;
+	int j;
+	char new;
+
+
+	while (s[i] != '\0')
+		i++;
+	
+	i = i - 1;
+
+	for (j = 0; j <= i / 2; j++)
+	{
+		new = s[j];
+		s[j] = s[i - j];
+		s[i - j] = new;
+	}
+}
