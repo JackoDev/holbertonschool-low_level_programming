7766c5436b693c8d11f99de2eb6aa3346c10ed87
untaljacko <untaljacko@gmail.com>
2019-07-04 13:57:28 -0500

    fixed: file 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,5 @@
 #include "holberton.h"
 /**
- * _strlen - returns the length of a string.
- * @s: the pointer to a first integer
- * Return: the length of a string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-/**
  * _strncpy - copy a string.
  * @dest: string of destiny
  * @src: string of source
@@ -22,11 +9,10 @@ int _strlen(char *s)
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i, tamd;
-	int tams = _strlen(src);
-
-	for (i = 0, tamd = 0; i <= n && src[i] != '\0'; i++, tamd++)
+	
+	for (i = 0, tamd = 0; i < n && src[i] != '\0'; i++, tamd++)
 		dest[tamd] = src[i];
-	for (i = tams ; i < n; i++)
+	for (; i < n; i++)
 		dest[i] = '\0';
 
 	return (dest);
