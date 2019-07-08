1579bdf1a08142095e78d7152407a0dc1d5bc341
untaljacko <untaljacko@gmail.com>
2019-07-08 09:47:35 -0500

    new files: holberton.h and task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ **_memset - fills memory with a constant byte.
+ * @s: memory area pointed
+ * @b: a constant byte
+ * @n: size of the bytes to fill
+ * Return: a pointer ti the s memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
