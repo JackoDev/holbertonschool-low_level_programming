2f4b1d6eb34271bd98027428acf547873f6d203a
untaljacko <untaljacko@gmail.com>
2019-07-09 17:40:55 -0500

    new file: task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - add two integers
+ * @i: first integer
+ * @j: second integer
+ * Return: the sum of the previous integers
+ */
+int add(int i, int j)
+{
+	int res = 0;
+
+	res = i + j;
+	return (res);
+}
