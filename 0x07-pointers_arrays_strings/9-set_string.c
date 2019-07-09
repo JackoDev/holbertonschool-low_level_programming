4418dda96a88d6fdb79c8587f9e03bf102f1b1b0
untaljacko <untaljacko@gmail.com>
2019-07-08 22:23:06 -0500

    new file: task 9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * set_string - prints the chessboard.
+ * @s: array to print
+ * @to: the size of the diagonal
+ * Return: its a void function
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
