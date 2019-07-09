73dc493e709eb8686b76580a343b71916d09a45d
untaljacko <untaljacko@gmail.com>
2019-07-08 21:53:48 -0500

    new file: task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stddef.h>
+/**
+ * print_chessboard - prints the chessboard.
+ * @a: array to print
+ *
+ * Return: its a void function
+ */
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		j = 0;
+		_putchar('\n');
+	}
+}
