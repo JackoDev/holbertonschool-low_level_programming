7930da049f61940fcac7da62c46771170bf7f229
untaljacko <untaljacko@gmail.com>
2019-07-14 18:37:57 -0500

    new file task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: argument number of parameters
+ * @argv: array with options inside the function
+ * Return: 1 or 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d/n", argc);
+	return (0);
+}
