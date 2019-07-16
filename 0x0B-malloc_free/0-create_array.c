6c8fcf8522cb0fcd1733930fc92afab9342310be
untaljacko <untaljacko@gmail.com>
2019-07-16 17:20:31 +0000

    fixed: task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * create_array - creates an array of chars, and initializes it with a specific char.
+ * create_array - creates an array of chars, and initializes it
+ * with a specific char.
  * @size: size of the array
  * @c: the specific char to initialize the array
  * Return: pointer to the new array
