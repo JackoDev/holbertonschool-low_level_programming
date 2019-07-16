e6993742e4ee1bd3eb4d3bc857c8856fa429cc90
untaljacko <untaljacko@gmail.com>
2019-07-16 16:51:26 +0000

    new file: Readme, task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars, and initializes it with a specific char.
+ * @size: size of the array
+ * @c: the specific char to initialize the array
+ * Return: pointer to the new array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+
+	str = malloc(size * sizeof(char));
+	while (i <= size)
+	{
+		str[i] = c;
+		i++;
+	}
+	if (str == NULL)
+		return (NULL);
+	else
+		return (str);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,6 @@
+" Let clangd fully control code completion
+let g:ycm_clangd_uses_ycmd_caching = 0
+" Use installed clangd, not YCM-bundled clangd which doesn't get updates.
+let g:ycm_clangd_binary_path = exepath("clangd")
+0x0B. C - malloc, free
+ Foundations - Low-level programming & Algorithm ― Hatching out
