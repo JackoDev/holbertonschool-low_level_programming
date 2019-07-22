66659482fdd0a9be2d342616b13672bbc8d6b5b6
untaljacko <untaljacko@gmail.com>
2019-07-22 16:53:38 +0000

    first commit new files: Readme and task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+0x0E. C - Structures, typedef
+ Foundations - Low-level programming & Algorithm ― Hatching out
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - a structure for the project
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif
