9e6008e01bf686d867299e9e810217202b087196
untaljacko <untaljacko@gmail.com>
2019-07-26 14:20:44 -0500

    new file: task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,19 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum all parameters
+ * @n: type of the arguments at least one
+ * Return: an integer
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list numbers;
+	int res = 0;
+
+	va_start(numbers, n);
+	for (i = 0; i < n; i++)
+		res =  res + va_arg(numbers, int);
+
+	va_end(numbers);
+	return (res);
+}
