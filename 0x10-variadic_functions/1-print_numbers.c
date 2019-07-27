0e2527d13461429b2a7a998211fdce3eb166bb5f
untaljacko <untaljacko@gmail.com>
2019-07-27 14:11:51 -0500

    fixed: order of declarations and insert if inside the loop

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,17 +8,16 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	int res = 0;
 	va_list numbers;
-
-	if (separator == NULL)
-		separator = "";
 
 	va_start(numbers, n);
 	for (i = 0; i < n; i++)
 	{
-		res = va_arg(numbers, int);
-		printf("%d%s\n", res, separator);
+		printf("%d", va_arg(numbers, int));
+
+		if (i < (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
+	printf("\n");
 	va_end(numbers);
 }
