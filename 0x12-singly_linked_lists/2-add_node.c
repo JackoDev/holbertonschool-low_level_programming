6e4e507c4d3d8e97cd99f260662115cff4f98adc
untaljacko <untaljacko@gmail.com>
2019-08-01 20:31:12 -0500

    proof function strdup

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,32 +14,6 @@ int _strlen(const char *s)
 	return (i);
 }
 /**
- * _strdup - duplicates a string
- * @str: string to dupplicate
- * Return: pointer to new string
- */
-char *_strdup(const char *str)
-{
-	char *copy;
-	unsigned int i, j;
-
-	if (str == NULL)
-		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	i++;
-	copy = malloc(i * sizeof(char));
-	if (copy == NULL)
-		return (NULL);
-	j = 0;
-	while (j < i)
-	{
-		copy[j] = str[j];
-		j++;
-	}
-	return (copy);
-}
-/**
  * add_node - radds a new node at the beginning of a list_t list.
  * @head: pointer to the list
  * @str: the str element of the first list element
@@ -53,7 +27,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (new == NULL)
 		return (NULL);
-	new->str = _strdup(str);
+	new->str = strdup(str);
 	new->len = _strlen(new->str);
 	new->next = (*head);
 	(*head) = new;
