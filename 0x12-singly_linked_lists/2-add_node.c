4902c6affd18c99c17ca0063ad379364cc3ce673
untaljacko <untaljacko@gmail.com>
2019-08-02 10:24:25 -0500

    include free in case malloc or strdup fails

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -32,7 +32,11 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	new->str = strdup(str);
 		if (new->str == NULL)
+		{
 			free(new->str);
+			free(new);
+			return (NULL);
+		}
 	new->len = _strlen(new->str);
 	new->next = (*head);
 	(*head) = new;
