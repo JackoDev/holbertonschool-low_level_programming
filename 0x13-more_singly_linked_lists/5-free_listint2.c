924f16b2794099fbf732b386a6f5fa5565d5896c
untaljacko <untaljacko@gmail.com>
2019-08-05 17:08:33 -0500

    new file: task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint2 - free a linked list.
+ * @head: pointer to the first element of the list
+ *
+ * Return: it's a void function
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *new;
+	listint_t *newtwo;
+
+	newtwo = *head;
+	while (head != NULL && newtwo != NULL)
+	{
+		new = newtwo;
+		newtwo = newtwo->next;
+		free(new);
+	}
+	*head = NULL;
+}
