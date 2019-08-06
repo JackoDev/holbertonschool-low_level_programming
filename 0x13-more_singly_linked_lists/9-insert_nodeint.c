90fe124e0fbc39f8bd28b6c22e47ba3ff9570cf3
untaljacko <untaljacko@gmail.com>
2019-08-06 14:38:39 -0500

    fixed: case for idx = NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t  *new, *make;
 	unsigned int index = 0;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (NULL);
 	new = *head;
 	if (idx == 0)
