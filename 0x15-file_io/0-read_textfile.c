20d1f1f7f60011bf7a2b3c531e16b8df49f44797
untaljacko <untaljacko@gmail.com>
2019-08-13 17:20:04 -0500

    new file: task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * read_textfile - reads a text file and prints it to the standard output.
+ * @filename: PAth of the file.
+ * @letters: number of letters it should read and print
+ * Return: number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	size_t countok;
+	int openfile, readfile, writefile;
+	char buffer;
+
+	openfile = open(filename, O_RDWR);
+		if (openfile == -1)
+			return (0);
+	readfile = read(openfile, &buffer, 1);
+		if (readfile == -1)
+			return (0);
+
+	for (countok = 0; readfile != 0 && countok < letters; countok++)
+	{
+		writefile = write(STDOUT_FILENO, &buffer, 1);
+			if (writefile == -1)
+				return (0);
+		readfile = read(openfile, &buffer, 1);
+			if (readfile == -1)
+				return (0);
+	}
+	readfile = close(openfile);
+		if (readfile == -1)
+			return (0);
+	return (countok);
+}
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif 
