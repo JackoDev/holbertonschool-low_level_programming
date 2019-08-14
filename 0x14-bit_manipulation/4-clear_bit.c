684a1874a6324311db229b849f92ada5f6008e8e
untaljacko <untaljacko@gmail.com>
2019-08-13 19:52:38 -0500

    new file: task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * clear_bit - sets the value of a bit to 1 at a given index.
+ * @n: the number to print in binary mode
+ * @index: index of the bit to review
+ * Return: 1 if works -1 if not
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int a = 1;
+
+	if (index > 63)
+		return (-1);
+
+	a = a << index;
+	*n = (*n & ~a);
+
+	return (1);
+}
