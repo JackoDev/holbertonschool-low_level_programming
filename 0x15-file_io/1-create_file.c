95818563294a2b0a88262685f63c40c2521f3838
untaljacko <untaljacko@gmail.com>
2019-08-13 19:08:47 -0500

    fixed: permissions file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,19 +12,20 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	openfile = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IWUSR);
+	openfile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 		if (openfile == -1)
 			return (-1);
 
-	if (text_content != NULL)
-	{
-		for (size = 0; text_content[size] != '\0'; size++)
+	if (text_content == NULL)
+		text_content = "";
+
+	for (size = 0; text_content[size] != '\0'; size++)
 		{
 		}
 
-		writefile = write(openfile, text_content, size);
-			if (writefile == -1 || writefile != size)
-				return (-1);
-	}
+	writefile = write(openfile, text_content, size);
+		if (writefile == -1 || writefile != size)
+			return (-1);
+	close(openfile);
 	return (1);
 }
