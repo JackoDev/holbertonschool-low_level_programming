df26be891b82918fe409afa9d9d6420da62948e2
untaljacko <untaljacko@gmail.com>
2019-09-13 10:22:30 -0500

    first commit, task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print the number of nodes in a double link list
+ * @h: the head of the double link list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	unsigned int counter = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		counter++;
+		h = h->next;
+	}
+	return(counter);
+}
