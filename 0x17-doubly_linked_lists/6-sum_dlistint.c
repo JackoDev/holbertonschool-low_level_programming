83f48082507db376c3d0919db31d8113f49e5a4c
untaljacko <untaljacko@gmail.com>
2019-09-15 13:55:05 -0500

    task 6: tested

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * sum_dlistint - return the sum of all de data nodes of double link list
+ * @head: the head of the double link list
+ * Return: the sum of the data
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	if (head->next == NULL && head->prev == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
