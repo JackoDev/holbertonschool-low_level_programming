6a407028821e41ece15651043f3cf716d5257571
untaljacko <untaljacko@gmail.com>
2019-09-15 12:30:35 -0500

    task 5: tested

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - return the node of the nth position in a link list
+ * @head: the head of the double link list
+ * @index: the position to search the node
+ * Return: the node required in the index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *tempo;
+	unsigned int counter = 0;
+
+	tempo = head;
+
+	if (head == NULL)
+		return (NULL);
+	if (index == 0 && (head->next == NULL && head->prev == NULL))
+		return (head);
+
+	while ((head != NULL) && (counter < index))
+	{
+		counter++;
+		tempo = tempo->next;
+	}
+	return (tempo);
+}
