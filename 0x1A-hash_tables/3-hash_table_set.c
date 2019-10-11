f230a467afbc863463bbc4316b21b4a383cdac40
untaljacko <untaljacko@gmail.com>
2019-10-11 18:57:54 -0500

    task 3: fixed comparison with null

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new, *tempo;
 	unsigned long int line;
 
-	if (key == "" || key == NULL || value == NULL || ht == NULL)
+	if (key == NULL || value == NULL || ht == NULL)
 		return (0);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
