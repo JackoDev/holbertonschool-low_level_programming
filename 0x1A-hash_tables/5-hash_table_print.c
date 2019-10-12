20a5932f45552ee2fe93ab8cfc84679aff7f3f1d
untaljacko <untaljacko@gmail.com>
2019-10-11 22:14:47 -0500

    task 5: partial tested ok

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - print the key and the value of the ht
+ * @ht: pointer to the ht
+ * Return: its a void function
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *tempo;
+	unsigned long int a = 0, b = 0;
+
+	if (ht == NULL)
+		return;
+	while (a < ht->size)
+	{
+		if (ht->array[a] != NULL)
+		{
+			tempo = ht->array[a];
+			while (tempo != NULL)
+			{
+				if (b == 1)
+					printf(", ");
+				printf("'%s': '%s'", tempo->key, tempo->value);
+				b = 1;
+				tempo = tempo->next;
+			}
+		}
+		a++;
+	}
+	printf("}\n");
+}
