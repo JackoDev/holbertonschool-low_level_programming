187c38f49891a2571e8c84302236e80635bdf352
untaljacko <untaljacko@gmail.com>
2020-01-16 15:17:17 +0000

    [fix] task 1, fixed betty and included edge cases

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,14 +10,19 @@
 int binary_rec(int arr[], int first, int last, int value)
 {
 	if (last >= first)
-	{	
+	{
 		int i, mid = first + (last - first) / 2;
-		
+
 		printf("Searching in array: ");
 		for (i = first; i < last; i++)
 			printf("%d, ", i);
 		printf("%d", last);
 		printf("\n");
+
+		if ((last == first) && (last != value))
+			return (-1);
+		if ((last == first) && (last == value))
+			return (0);
 
 		if (arr[mid] == value)
 			return (mid);
@@ -38,14 +43,14 @@ int binary_rec(int arr[], int first, int last, int value)
 int binary_search(int *array, size_t size, int value)
 {
 	int first, last, index;
-	
+
 	if (array == NULL)
 		return (-1);
 
 	first = array[0];
 	last = array[size - 1];
 	index = binary_rec(array, first, last, value);
-	
+
 	if (index == -1)
 		return (-1);
 
