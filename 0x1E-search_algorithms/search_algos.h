3984ce6efe5f2dd29c089826296b6e87b5339d3d
untaljacko <untaljacko@gmail.com>
2020-01-16 14:54:10 +0000

    [new] task 1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,7 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int binary_rec(int arr[], int first, int last, int value);
 
 #endif
